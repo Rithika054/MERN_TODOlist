76eb13a35511e876f6d80aaf61ca0974471d0fb4
RITHIKA <129405332+Rithika054@users.noreply.github.com>
2024-03-25 21:36:23 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(cors(
     {
         origin : '*',
-        methods: ["POST","GET","UPDATE","DELETE"],
+        methods: ["POST","GET","PUT","DELETE"],
         credentials:true
     }));
 app.use(express.json())
