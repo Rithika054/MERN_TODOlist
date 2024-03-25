cf31e624da0ea02914dc3d24c06eb9a3ed93e3f6
RITHIKA <129405332+Rithika054@users.noreply.github.com>
2024-03-25 21:29:28 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const TodoModel = require('./Models/Todo')
 const app=express()
 app.use(cors(
     {
-        origin : ["https://mern-todolist-frontend-eight.vercel.app"],
+        origin : '*',
         methods: ["POST","GET"],
         credentials:true
     }));
